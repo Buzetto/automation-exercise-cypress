1273e04c9b99744b3b7048efbdea49e84cab436b
Victor Buzetto <victoraugustobuzetto@gmail.com>
2026-04-24 21:42:34 -0300

    Refatorando o código e deixando com Page Object e métodos ao invés da massa de teste toda nos arquivos de testes

--- cypress/e2e/01-cadastro.cy.js
+++ cypress/e2e/01-cadastro.cy.js
@@ -1,96 +1,32 @@
-describe ("Testes de cadastro", () => {
-    let massa;
+import data from '../../cypress/fixtures/test-data.json'
+
+describe ("Login and Register Account", () => {
 
     beforeEach (() => {
-        cy.fixture('test-data').then((dados) => {
-            massa = dados;
-        });
-
-        cy.visit('https://automationexercise.com/')
-        cy.get('.logo').should('be.visible')
+        cy.start()
     })
 
     it("TC_01 - Register User", () => {
-        cy.get('a[href="/login"]').click()
-        cy.contains('h2', 'New User Signup!').should('be.visible')
-
-        cy.get('[data-qa="signup-name"]').type(massa.temporary_account.first_name)
-        cy.get('[data-qa="signup-email"]').type(massa.temporary_account.email)
-        cy.get('[data-qa="signup-button"]').click()
-        cy.contains('b', 'Enter Account Information')
-
-        cy.get('#id_gender1').check()
-        cy.get('[data-qa="password"]').type(Cypress.env('system_password'))
-        cy.get('[data-qa="days"]').select(massa.temporary_account.birthday_day)
-        cy.get('[data-qa="months"]').select(massa.temporary_account.birthday_month)
-        cy.get('[data-qa="years"]').select(massa.temporary_account.birthday_year)
-
-        cy.get('#newsletter').check()
-        cy.get('#optin').check()
-
-        cy.get('[data-qa="first_name"]').type(massa.temporary_account.first_name)
-        cy.get('[data-qa="last_name"]').type(massa.temporary_account.last_name)
-        cy.get('[data-qa="company"]').type(massa.temporary_account.company)
-        cy.get('[data-qa="address"]').type(massa.temporary_account.address)
-        cy.get('[data-qa="country"]').select(massa.temporary_account.country)
-        cy.get('[data-qa="state"]').type(massa.temporary_account.state)
-        cy.get('[data-qa="city"]').type(massa.temporary_account.city)
-        cy.get('[data-qa="zipcode"]').type(massa.temporary_account.zipcode)
-        cy.get('[data-qa="mobile_number"]').type(massa.temporary_account.mobile_number)
-
-        cy.get('[data-qa="create-account"]').click()
-        cy.get('[data-qa="account-created"]').should('be.visible')
-        cy.get('[data-qa="continue-button"]').click()
-
-        cy.contains('a', 'Logged in as').should('be.visible')
-
-        cy.get('a[href="/delete_account"]').click()
-        cy.get('[data-qa="account-deleted"]').should('be.visible')
-        cy.get('[data-qa="continue-button"]').click()
+        cy.registerAccount(data.temporary_account)
+        cy.deleteAccount()
     })
 
     it("TC_02 - Login User with correct email and password", () => {
-        cy.get('a[href="/login"]').click()
-        cy.contains('h2', 'Login to your account').should('be.visible')
-
-        cy.get('[data-qa="login-email"]').type(massa.permanent_account.email)
-        cy.get('[data-qa="login-password"]').type(Cypress.env('system_password'))
-
-        cy.get('[data-qa="login-button"]').click()
-        cy.contains('a', 'Logged in as').should('be.visible')
+        cy.permanentAccount(data.permanent_account)
     })
 
     it("TC_03 - Login User with incorrect email and password", () => {
-        cy.get('a[href="/login"]').click()
-        cy.contains('h2', 'Login to your account').should('be.visible')
-
-        cy.get('[data-qa="login-email"]').type(massa.wrong_login.email)
-        cy.get('[data-qa="login-password"]').type(Cypress.env('system_password'))
-
-        cy.get('[data-qa="login-button"]').click()
-        cy.contains('p', 'Your email or password is incorrect!').should('be.visible')
+        cy.wrongAccount(data.wrong_login)
     })
 
     it("TC_04 - Logout User", () => {
-        cy.get('a[href="/login"]').click()
-        cy.contains('h2', 'Login to your account').should('be.visible')
-
-        cy.get('[data-qa="login-email"]').type(massa.permanent_account.email)
-        cy.get('[data-qa="login-password"]').type(Cypress.env('system_password'))
-        cy.get('[data-qa="login-button"]').click()
-        cy.contains('a', 'Logged in as').should('be.visible')
+        cy.permanentAccount(data.permanent_account)
 
         cy.get('a[href="/logout"]').click()
         cy.get('.logo').should('be.visible')
     })
 
     it("TC_05 - Register User with existing email", () => {
-        cy.get('a[href="/login"]').click()
-        cy.contains('h2', 'New User Signup!').should('be.visible')
-
-        cy.get('[data-qa="signup-name"]').type(massa.permanent_account.name)
-        cy.get('[data-qa="signup-email"]').type(massa.permanent_account.email)
-        cy.get('[data-qa="signup-button"]').click()
-        cy.contains('p', 'Email Address already exist!').should('be.visible')
+        cy.accountExist(data.permanent_account)
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,87 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("start", () => {
+  cy.viewport("macbook-16");
+  cy.visit("https://automationexercise.com/");
+  cy.get(".logo").should("be.visible");
+});
+
+//Register Account
+Cypress.Commands.add("registerAccount", (userData) => {
+  cy.get('a[href="/login"]').click();
+  cy.contains("h2", "New User Signup!").should("be.visible");
+
+  cy.get('[data-qa="signup-name"]').type(userData.first_name);
+  cy.get('[data-qa="signup-email"]').type(userData.email);
+  cy.get('[data-qa="signup-button"]').click();
+  cy.contains("b", "Enter Account Information");
+
+  cy.get("#id_gender1").check();
+  cy.get('[data-qa="password"]').type(Cypress.env("system_password"));
+  cy.get('[data-qa="days"]').select(userData.birthday_day);
+  cy.get('[data-qa="months"]').select(userData.birthday_month);
+  cy.get('[data-qa="years"]').select(userData.birthday_year);
+
+  cy.get("#newsletter").check();
+  cy.get("#optin").check();
+
+  cy.get('[data-qa="first_name"]').type(userData.first_name);
+  cy.get('[data-qa="last_name"]').type(userData.last_name);
+  cy.get('[data-qa="company"]').type(userData.company);
+  cy.get('[data-qa="address"]').type(userData.address);
+  cy.get('[data-qa="country"]').select(userData.country);
+  cy.get('[data-qa="state"]').type(userData.state);
+  cy.get('[data-qa="city"]').type(userData.city);
+  cy.get('[data-qa="zipcode"]').type(userData.zipcode);
+  cy.get('[data-qa="mobile_number"]').type(userData.mobile_number);
+
+  cy.get('[data-qa="create-account"]').click();
+  cy.get('[data-qa="account-created"]').should("be.visible");
+  cy.get('[data-qa="continue-button"]').click();
+
+  cy.contains("a", "Logged in as").should("be.visible");
+});
+
+//Login with an account created
+Cypress.Commands.add("permanentAccount", (userDataCorrect) => {
+  cy.get('a[href="/login"]').click();
+  cy.contains("h2", "Login to your account").should("be.visible");
+
+  cy.get('[data-qa="login-email"]').type(userDataCorrect.email);
+  cy.get('[data-qa="login-password"]').type(Cypress.env("system_password"));
+
+  cy.get('[data-qa="login-button"]').click();
+  cy.contains("a", "Logged in as").should("be.visible");
+});
+
+//Login with a wrong account
+Cypress.Commands.add("wrongAccount", (userDataWrong) => {
+  cy.get('a[href="/login"]').click();
+  cy.contains("h2", "Login to your account").should("be.visible");
+
+  cy.get('[data-qa="login-email"]').type(userDataWrong.email);
+  cy.get('[data-qa="login-password"]').type(Cypress.env("system_password"));
+
+  cy.get('[data-qa="login-button"]').click();
+  cy.contains("p", "Your email or password is incorrect!").should("be.visible");
+});
+
+//Register with an email that already exist
+Cypress.Commands.add("accountExist", (userDataExist) => {
+  cy.get('a[href="/login"]').click();
+  cy.contains("h2", "New User Signup!").should("be.visible");
+
+  cy.get('[data-qa="signup-name"]').type(userDataExist.name);
+  cy.get('[data-qa="signup-email"]').type(userDataExist.email);
+  cy.get('[data-qa="signup-button"]').click();
+  cy.contains("p", "Email Address already exist!").should("be.visible");
+});
+
+//Delete Account
+Cypress.Commands.add("deleteAccount", () => {
+  cy.get('a[href="/delete_account"]').click();
+  cy.get('[data-qa="account-deleted"]').should("be.visible");
+  cy.get('[data-qa="continue-button"]').click();
+});
